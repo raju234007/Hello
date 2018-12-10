3330df4f3056ef1984540d05e4cf20f4044aee49
Raju_Manikala <raju.manikala@csgsol.com>
2018-12-10 16:35:49 +0530

    modified to actor

--- flywayscripts/src/migrations/V1_9__flyway_sprint_Sharmad_Pasha.sql
+++ flywayscripts/src/migrations/V1_9__flyway_sprint_Sharmad_Pasha.sql
@@ -2,12 +2,12 @@
 -- author : Raju
 
 CREATE TABLE IF NOT EXISTS pasha (
-    emp_id INT AUTO_INCREMENT,
+    actor_id INT AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     start_date DATE,
     due_date DATE,
     status TINYINT NOT NULL,
     priority TINYINT NOT NULL,
     movie_name TEXT,
-    PRIMARY KEY (task_id)
+    PRIMARY KEY (actor_id)
 )  ENGINE=INNODB;
